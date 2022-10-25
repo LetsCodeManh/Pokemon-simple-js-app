72a2681ea0b1db6a9dde723dd9306c2cfa8a4ccf
LetsCodeManh <manh.ng@icloud.com>
2022-10-25 22:48:27 +0200

    Create a variable in JS
    pokemonList variable with 3 Pokemon.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let pokemonList = [
+  {
+    index: "#001",
+    name: "Bulbasaur",
+    height: "2' 04\"",
+    type: ["grass", "poison"],
+    weaknesses: ["fire", "psychic", "flying", "ice"],
+  },
+  {
+    index: "#002",
+    name: "Ivysaur",
+    height: "3' 03\"",
+    type: ["grass", "poison"],
+    weaknesses: ["fire", "psychic", "flying", "ice"],
+  },
+  {
+    index: "#003",
+    name: "Venusaur",
+    height: "6' 07\"",
+    type: ["grass", "poison"],
+    weaknesses: ["fire", "psychic", "flying", "ice"],
+  },
+];
+
+console.log(pokemonList);
