f9e0888af76ab4a83e5e737a567139fd997a0bad
LetsCodeManh <manh.ng@icloud.com>
2022-10-26 11:11:10 +0200

    Write another loop for pokemonList
    Also checking if the height is big or small

--- js/script.js
+++ js/script.js
@@ -20,10 +20,36 @@ let pokemonList = [
     type: ["grass", "poison"],
     weaknesses: ["fire", "psychic", "flying", "ice"],
   },
+  {
+    index: "#999",
+    name: "XXX",
+    height: "-1",
+    type: ["grass", "poison"],
+    weaknesses: ["fire", "psychic", "flying", "ice"],
+  }
 ];
 
 // Write a pokemonList in JS - name + height
 for (i = 0; i < pokemonList.length; i++) {
   document.write(`${pokemonList[i].name} ${pokemonList[i].height}`);
   console.log(`${pokemonList[i].name} ${pokemonList[i].height}`);
+}
+
+// Write a pokemonList in JS and checking the height of the pokemon- name + height
+for (i = 0; i < pokemonList.length; i++) {
+  if (parseFloat(pokemonList[i].height) >= 6) {
+    console.log(
+      `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a big one`
+    );
+  } else if (parseFloat(pokemonList[i].height)  >= 3) {
+    console.log(
+      `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a normal one`
+    );
+  } else if (parseFloat(pokemonList[i].height)  >= 0) {
+    console.log(
+      `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a small one`
+    );
+  } else {
+    console.log('This Pokemon height doesnt exits')
+  }
 }
