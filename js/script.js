bc9b1899911a0ed2e9ea0ac0c9ff2b9734d4d166
LetsCodeManh <manh.ng@icloud.com>
2022-10-27 13:58:56 +0200

    Deleted comments

--- js/script.js
+++ js/script.js
@@ -45,12 +45,6 @@ let pokemonRepository = (function () {
     }
   }
 
-  // function pokemonSearch(arr, query) {
-  //   return arr.filter(pokemon => {
-  //     pokemon.toLowerCase().includes(query.toLowerCase())
-  //   })
-  // }
-
   return {
     getAll: getAll,
     add: add,
@@ -64,38 +58,3 @@ pokemonRepository.getAll().forEach((pokemon) => {
   document.write("<p>" + pokemon.name + " " + pokemon.height + "</p>");
 });
 
-// Write a pokemonList in JS - name + height
-// for (i = 0; i < pokemonList.length; i++) {
-//   document.write(
-//     "<p>" + pokemonList[i].name + " " + pokemonList[i].height + "</p>"
-//   );
-//   console.log(`${pokemonList[i].name} ${pokemonList[i].height}`);
-// }
-
-// Write a pokemonList in JS and checking the height of the pokemon- name + height
-// for (i = 0; i < pokemonList.length; i++) {
-//   if (parseFloat(pokemonList[i].height) >= 6) {
-//     document.write(
-//       "<p>" + pokemonList[i].name + " " + pokemonList[i].height + " - Wow, that is a big one" + "</p>"
-//     );
-//     console.log(
-//       `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a big one`
-//     );
-//   } else if (parseFloat(pokemonList[i].height) >= 3) {
-//     document.write(
-//       "<p>" + pokemonList[i].name + " " + pokemonList[i].height + " - Wow, that is a normal one" + "</p>"
-//     );
-//     console.log(
-//       `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a normal one`
-//     );
-//   } else if (parseFloat(pokemonList[i].height) >= 0) {
-//     document.write(
-//       "<p>" + pokemonList[i].name + " " + pokemonList[i].height + " - Wow, that is a small one" + "</p>"
-//     );
-//     console.log(
-//       `${pokemonList[i].name} ${pokemonList[i].height} - Wow, that is a small one`
-//     );
-//   } else {
-//     console.log("This Pokemon height doesnt exits");
-//   }
-// }
